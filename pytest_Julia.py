e7aa640f3d3725c58d570bdd73638fe1c83193b7
MeiYuusaki <94790055+MeiYuusaki@users.noreply.github.com>
2024-02-03 23:24:35 +0800

    Assignment 2 Part 2 complete

--- pytest_Julia.py
+++ pytest_Julia.py
@@ -2,7 +2,8 @@ import pytest
 from JuliaSet import calc_pure_python
 
 def test_julia():
-    assert calc_pure_python(desired_width=1000, max_iterations=300) == 33219980
+    summation, _ = calc_pure_python(desired_width=1000, max_iterations=300)
+    assert summation == 33219980
 
 # How would you implement the unit test with the possibility of having a different number of iterations and grid points?
 '''
