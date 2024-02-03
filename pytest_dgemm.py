94c558042680547210ee7e94d785fb4ef9722edc
MeiYuusaki <94790055+MeiYuusaki@users.noreply.github.com>
2024-02-04 02:10:43 +0800

    Assignment 2 Part 3 complete

---
+++ pytest_dgemm.py
@@ -0,0 +1,10 @@
+import pytest
+from scipy.linalg import blas
+from dgemm import run
+import numpy as np
+
+def test_dgemm():
+    N, A, B, pred = run(5)
+    exp = np.zeros((N, N), dtype=np.float64)
+    exp = blas.dgemm(1.0, A, B, 0.0, exp)
+    assert np.allclose(exp, pred)
