963b3e4c9764f32cee75dc975602e84045c4245a
MeiYuusaki <94790055+MeiYuusaki@users.noreply.github.com>
2024-02-03 23:16:29 +0800

    Assignment 2 Part 2 done

---
+++ pytest_Julia.py
@@ -0,0 +1,11 @@
+import pytest
+from JuliaSet import calc_pure_python
+
+def test_julia():
+    assert calc_pure_python(desired_width=1000, max_iterations=300) == 33219980
+
+# How would you implement the unit test with the possibility of having a different number of iterations and grid points?
+'''
+Compare the current implementation of the Julia Set Code with 
+reference implementations or established libraries that have the Julia Set Code implementations.
+'''
